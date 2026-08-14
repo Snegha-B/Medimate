c81bdf73a54fdfc28fe981ac6974f8ffc473b9dc
Snegha B <rakshitha0186@gmail.com>
2026-08-14 20:21:59 +0530

    Register MediMate models in Django admin

--- backend/core/admin.py
+++ backend/core/admin.py
@@ -1,3 +1,36 @@
 from django.contrib import admin
+from .models import (
+    Prescription,
+    Medication,
+    Schedule,
+    DoseLog,
+    PushSubscription,
+    ReferenceRange,
+    LabReport,
+    LabValue,
+    ShareToken,
+    UserProfile,
+    Caregiver,
+    Appointment,
+    Notification,
+    MedicineReminder,
+    NotificationPreference,
+    FCMDevice,
+)
 
-# Register your models here.
+admin.site.register(Prescription)
+admin.site.register(Medication)
+admin.site.register(Schedule)
+admin.site.register(DoseLog)
+admin.site.register(PushSubscription)
+admin.site.register(ReferenceRange)
+admin.site.register(LabReport)
+admin.site.register(LabValue)
+admin.site.register(ShareToken)
+admin.site.register(UserProfile)
+admin.site.register(Caregiver)
+admin.site.register(Appointment)
+admin.site.register(Notification)
+admin.site.register(MedicineReminder)
+admin.site.register(NotificationPreference)
+admin.site.register(FCMDevice)
